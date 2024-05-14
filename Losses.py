166534af6385cc349ef6337eedcd3a716708f4f2
Latheuni <lauren.theunissen@gmail.com>
2024-05-14 16:37:51 +0200

    Added Cross Entropy Loss

--- Losses.py
+++ Losses.py
@@ -6,7 +6,7 @@ import torch.nn.functional as F
 
 # source: OpenOOD
 class LogitNormLoss(nn.Module):
-    def __init__(self, tau=0.04): # 0.04 is used in the paper and benchmark
+    def __init__(self, tau=0.04):  # 0.04 is used in the paper and benchmark
         super(LogitNormLoss, self).__init__()
         self.tau = tau
 
@@ -14,3 +14,11 @@ class LogitNormLoss(nn.Module):
         norms = torch.norm(x, p=2, dim=-1, keepdim=True) + 1e-7
         logit_norm = torch.div(x, norms) / self.tau
         return F.cross_entropy(logit_norm, target)  # Should be in the NN losses
+
+
+class CrossEntropy(nn.Module):
+    def __init__(self):
+        super(CrossEntropy, self).__init__()
+
+    def forward(self, x, target):
+        return F.cross_entropy(x, target)
